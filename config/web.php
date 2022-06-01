0982c6521f650386ed0efbaeddd95b8ff7ece3e8
yiyanyun <488787536@qq.com>
2022-06-01 15:55:53 +0800

    修复云端接口API

--- config/web.php
+++ config/web.php
@@ -1,5 +1,11 @@
 <?php 
 /*
+ * @Author       : Lucifer
+ * @Date         : 2022-04-10 10:47:50
+ * @LastEditTime : 2022-06-01 15:54:22
+ * @FilePath     : \yiyanyun1.2\config\web.php
+ */
+/*
  * @Author: Lucifer
  * @Date: 2022-03-13 17:10:00
  * @LastEditTime: 2022-04-10 10:47:28
@@ -7,6 +13,5 @@
  */
 return [
     'version' => '1.2.0',
-    // 'yun_api' => 'http://172.17.204.30/api/',
-    'yun_api' => 'http://175.178.189.132/api/',
+    // 'yun_api' => '当前版本暂不支持对接云端请关注下一版本',
 ];
